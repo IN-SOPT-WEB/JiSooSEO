421715f746444f94dc83df145b6a60a7eca3bb0b
Seojisoo20191941 <amy0480@naver.com>
2022-10-12 10:20:31 +0900

    feat : 버튼에 따라 레이아웃 달리하기 구현 완료

--- assignment/week2/todolist/main.js
+++ assignment/week2/todolist/main.js
@@ -1,5 +1,29 @@
 const seeToday = document.querySelector("#see-today-button");
 const seeTomorrow = document.querySelector("#see-tomorrow-button");
-const seeTogether = document.querySelector("#see-toygether-button");
+const seeTogether = document.querySelector("#see-together-button");
 
-// if (seeToday.)
+const leftSection = document.querySelector("#left-section");
+const rightSection = document.querySelector("#right-section");
+
+function seeTodayClick() {
+  leftSection.style.width = "0";
+  leftSection.style.display = "none";
+  rightSection.style.width = "30vw";
+  rightSection.style.display = "flex";
+}
+function seeTomorrowClick() {
+  rightSection.style.width = "0";
+  rightSection.style.display = "none";
+  leftSection.style.width = "30vw";
+  leftSection.style.display = "flex";
+}
+function seeTogetherClick() {
+  rightSection.style.width = "30vw";
+  rightSection.style.display = "flex";
+  leftSection.style.width = "30vw";
+  leftSection.style.display = "flex";
+}
+
+seeToday.addEventListener("click", seeTodayClick);
+seeTomorrow.addEventListener("click", seeTomorrowClick);
+seeTogether.addEventListener("click", seeTogetherClick);
