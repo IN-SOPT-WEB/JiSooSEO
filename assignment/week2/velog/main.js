be3b50d38ccd497cb4e3cf1e68abd55d669ed202
Seojisoo20191941 <amy0480@naver.com>
2022-10-13 00:21:24 +0900

    feat : 토글링 아이템 클릭시 토글 닫히게

--- assignment/week2/velog/main.js
+++ assignment/week2/velog/main.js
@@ -1,11 +1,8 @@
 const dateList = document.querySelector("#select-date-list");
 const dateSelectButton = document.querySelector("#select-default-date");
 
-// const day = document.querySelector("#select-day");
-// const week = document.querySelector("#select-week");
-// const month = document.querySelector("#select-month");
-// const year = document.querySelector("#select-year");
 const selecting = document.querySelectorAll(".selecting");
+const defaultDate = document.querySelector("#select-default-date-text");
 
 function showDateList() {
   let dateListStatus = dateList.style.display;
@@ -18,6 +15,9 @@ function showDateList() {
   for (let i = 0; i < selecting.length; i++) {
     selecting[i].addEventListener("click", function () {
       selecting[i].classList.add("date-font-color");
+      defaultDate.innerHTML = selecting[i].innerHTML;
+      document.querySelector("#select-date-list").style.display = "none";
+
       for (let j = 0; j < selecting.length; j++) {
         if (j !== i) {
           selecting[j].classList.remove("date-font-color");
