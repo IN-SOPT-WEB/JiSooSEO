b87cdeed9bc393b9384e27fed2b18ff29144df99
Seojisoo20191941 <amy0480@naver.com>
2022-10-12 10:22:22 +0900

    feat : 버튼에 따라 레이아웃 달리하기 구현 완료

--- assignment/week2/todolist/main.js
+++ assignment/week2/todolist/main.js
@@ -8,13 +8,13 @@ const rightSection = document.querySelector("#right-section");
 function seeTodayClick() {
   leftSection.style.width = "0";
   leftSection.style.display = "none";
-  rightSection.style.width = "30vw";
+  rightSection.style.width = "60vw";
   rightSection.style.display = "flex";
 }
 function seeTomorrowClick() {
   rightSection.style.width = "0";
   rightSection.style.display = "none";
-  leftSection.style.width = "30vw";
+  leftSection.style.width = "60vw";
   leftSection.style.display = "flex";
 }
 function seeTogetherClick() {
