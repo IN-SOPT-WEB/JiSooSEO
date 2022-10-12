4432175a6ad43cf09501279062e9ac3a3868e8c3
Seojisoo20191941 <amy0480@naver.com>
2022-10-12 10:08:14 +0900

    style : 버튼 hover시 색깔 바꾸기

--- assignment/week2/todolist/main.js
+++ assignment/week2/todolist/main.js
@@ -1,5 +1,5 @@
-// const seeToday = document.querySelector("#see-today-button");
-// const seeTomorrow = document.querySelector("#see-tomorrow-button");
-// const seeTogether = document.querySelector("#see-toygether-button");
+const seeToday = document.querySelector("#see-today-button");
+const seeTomorrow = document.querySelector("#see-tomorrow-button");
+const seeTogether = document.querySelector("#see-toygether-button");
 
 // if (seeToday.)
