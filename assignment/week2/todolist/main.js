f1a24a137e23bfa7287b1c05bd3b1dc58ecb7bd9
Seojisoo20191941 <amy0480@naver.com>
2022-10-12 10:59:40 +0900

    feat : 투두리스트 오늘할일 추가하기 구현 완료

--- assignment/week2/todolist/main.js
+++ assignment/week2/todolist/main.js
@@ -1,3 +1,4 @@
+// 버튼에 따라 레이아웃 달리하기 구현
 const seeToday = document.querySelector("#see-today-button");
 const seeTomorrow = document.querySelector("#see-tomorrow-button");
 const seeTogether = document.querySelector("#see-together-button");
@@ -23,7 +24,50 @@ function seeTogetherClick() {
   leftSection.style.width = "30vw";
   leftSection.style.display = "flex";
 }
+// 할 일 추가, 삭제
+const addTodayTaskButton = document.querySelector("#addTodayTaskButton");
+const addTomorrowTaskButton = document.querySelector("#addTomorrowTaskButton");
+const tomorrowTask = document.querySelector("#addTomorrowTask").value;
+
+function addTodayTask() {
+  const addTodayValue = document.querySelector("#addTodayTask").value;
+
+  const li = document.createElement("li");
+  const input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+
+  const button = document.createElement("button");
+  li.setAttribute("id", addTodayValue);
+
+  const span = document.createElement("span");
+  span.innerHTML = addTodayValue;
+  span.setAttribute("class", "todo-text");
+  //   const textNode = document.createTextNode(addTodayValue);
+  li.appendChild(input);
+  //   li.appendChild(textNode);
+  li.appendChild(span);
+  li.appendChild(button);
+  button.innerText = "trash";
+  button.setAttribute("class", "trash-button");
+  //   button.addEventListener("click", deleteTasks);
+
+  document.querySelector("#todayTaskList").appendChild(li);
+}
+function addTomorrowTask() {
+  const addTomorrowValue = document.querySelector("#addTomorrowTask").value;
+
+  const li = document.createElement("li");
+  li.setAttribute("id", addTomorrowValue);
+
+  const textNode = document.createTextNode(addTomorrowValue);
+  li.appendChild(textNode);
+
+  document.querySelector("#tomorrowTaskList").appendChild(li);
+}
 
 seeToday.addEventListener("click", seeTodayClick);
 seeTomorrow.addEventListener("click", seeTomorrowClick);
 seeTogether.addEventListener("click", seeTogetherClick);
+
+addTodayTaskButton.addEventListener("click", addTodayTask);
+addTomorrowTaskButton.addEventListener("click", addTomorrowTask);
