682f246fbed091a2349b60c31625d45aa74b512e
Seojisoo20191941 <amy0480@naver.com>
2022-10-12 09:54:34 +0900

    refactor : week1코드 리팩토링. 버튼 타입 지정, header와 footer컬러 추가, 섹션 테두리 생성

---
+++ assignment/week2/todolist/main.js
@@ -0,0 +1,5 @@
+// const seeToday = document.querySelector("#see-today-button");
+// const seeTomorrow = document.querySelector("#see-tomorrow-button");
+// const seeTogether = document.querySelector("#see-toygether-button");
+
+// if (seeToday.)
