532663e513c2964cd3d3fbdddef070ad05ee12e6
Seojisoo20191941 <amy0480@naver.com>
2022-10-13 00:17:15 +0900

    feat : 토글링 아이템 클릭시 font-color 변경

--- assignment/week2/velog/main.js
+++ assignment/week2/velog/main.js
@@ -1,63 +1,11 @@
-// const selectDateButton = document.querySelector("#select-date-button");
-
-// const selectDay = document.querySelector("#select-day");
-// const selectWeek = document.querySelector("#select-week");
-// const selectMonth = document.querySelector("#select-month");
-// const selectYear = document.querySelector("#select-year");
-
-// const defaultDate = document.querySelector("#select-default-date");
-// const dateList = document.querySelector("#select-date-list");
-
-// function selectingDate() {
-//   let p_num = 0;
-//   if (document.querySelector(".data-list-showing") !== null) {
-//     p_num += 1;
-//   }
-//   if (p_num === 0) {
-//     console.log("adsfsaf a");
-//     const day = document.createElement("p");
-//     const week = document.createElement("p");
-//     const month = document.createElement("p");
-//     const year = document.createElement("p");
-//     day.innerHTML = "오늘";
-//     week.innerHTML = "이번 주";
-//     month.innerHTML = "이번 달";
-//     year.innerHTML = "올해";
-//     // dateList.style.display = "block";
-
-//     day.setAttribute("class", "data-list-showing");
-//     week.setAttribute("class", "data-list-showing");
-//     month.setAttribute("class", "data-list-showing");
-//     year.setAttribute("class", "data-list-showing");
-
-//     dateList.appendChild(day);
-//     dateList.appendChild(week);
-//     dateList.appendChild(month);
-//     dateList.appendChild(year);
-//     console.log("p_num" + p_num);
-//   } else {
-//     console.log("Adsfasfdsafdsafds");
-//     dateList.style.display = "none";
-//     p_num = 0;
-//     dateList.removeChild(day);
-//     dateList.removeChild(week);
-//     dateList.removeChild(month);
-//     dateList.removeChild(year);
-//   }
-// }
-// // function selectDateButton(){
-// //     if
-// // }
-// selectDateButton.addEventListener("click", selectingDate);
-
-// // const selectDateButton = document.querySelector("#select-date-button");
-// // function selectDate() {
-// //   console.log("adfaefdafewqfq");
-// // }
-// // selectDateButton.addEventListener("click", selectDate);
-
 const dateList = document.querySelector("#select-date-list");
 const dateSelectButton = document.querySelector("#select-default-date");
+
+// const day = document.querySelector("#select-day");
+// const week = document.querySelector("#select-week");
+// const month = document.querySelector("#select-month");
+// const year = document.querySelector("#select-year");
+const selecting = document.querySelectorAll(".selecting");
 
 function showDateList() {
   let dateListStatus = dateList.style.display;
@@ -66,5 +14,20 @@ function showDateList() {
   } else {
     document.querySelector("#select-date-list").style.display = "block";
   }
+  console.log(selecting);
+  for (let i = 0; i < selecting.length; i++) {
+    selecting[i].addEventListener("click", function () {
+      selecting[i].classList.add("date-font-color");
+      for (let j = 0; j < selecting.length; j++) {
+        if (j !== i) {
+          selecting[j].classList.remove("date-font-color");
+        }
+      }
+    });
+  }
+
+  //   day.classList.add("date-font-color");
+  //   console.log()
 }
+
 dateSelectButton.addEventListener("click", showDateList);
