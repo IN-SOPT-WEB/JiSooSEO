ea399420d51f0688e07168ecdf1cf0f5f4df43d2
Seojisoo20191941 <amy0480@naver.com>
2022-10-14 02:09:03 +0900

    feat : 모달 구현 시작. 카드 클릭 시, 깊은 복사되고 x버튼 생성. x버튼 클릭 시 모달 없어지는 기능 구현.

--- assignment/week2/velog/main.js
+++ assignment/week2/velog/main.js
@@ -27,4 +27,35 @@ function showDateList() {
   }
 }
 
+const cards = document.querySelectorAll(".card");
+let idNum = 0;
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("click", function () {
+    console.log("ADfasdfsafdsafds");
+    const cardModal = document.createElement("div");
+    //복사할 카드
+    const cardOrigin = cards[i];
+    //카드 복사하기 - 깊은 복사
+    const cardCopy = cardOrigin.cloneNode(true);
+    idNum++;
+    cardCopy.id = "copyCard" + idNum;
+    cardOrigin.after(cardCopy);
+    //x버튼 만들기
+    const xButton = document.createElement("button");
+    xButton.innerHTML = "x";
+
+    cardModal.appendChild(cardOrigin);
+    cardModal.appendChild(xButton);
+
+    document.querySelector("body").appendChild(cardModal);
+
+    xButton.addEventListener("click", deleteX);
+  });
+}
+function deleteX(e) {
+  const deleteX = e.target.parentElement;
+
+  deleteX.remove();
+}
+
 dateSelectButton.addEventListener("click", showDateList);
