291cfa43ee60c9213d036e601d0e12b8e77cb487
Seojisoo20191941 <amy0480@naver.com>
2022-10-14 02:49:03 +0900

    style : 모달창, 오버레이 구현. 모달창 뒤 스크롤 막음

--- assignment/week2/velog/main.js
+++ assignment/week2/velog/main.js
@@ -28,17 +28,15 @@ function showDateList() {
 }
 
 const cards = document.querySelectorAll(".card");
-let idNum = 0;
 for (let i = 0; i < cards.length; i++) {
   cards[i].addEventListener("click", function () {
     console.log("ADfasdfsafdsafds");
     const cardModal = document.createElement("div");
+    cardModal.setAttribute("class", "card-modal-style");
     //복사할 카드
     const cardOrigin = cards[i];
     //카드 복사하기 - 깊은 복사
     const cardCopy = cardOrigin.cloneNode(true);
-    idNum++;
-    cardCopy.id = "copyCard" + idNum;
     cardOrigin.after(cardCopy);
     //x버튼 만들기
     const xButton = document.createElement("button");
@@ -47,7 +45,11 @@ for (let i = 0; i < cards.length; i++) {
     cardModal.appendChild(cardOrigin);
     cardModal.appendChild(xButton);
 
-    document.querySelector("body").appendChild(cardModal);
+    document.body.appendChild(cardModal);
+    document.body.classList.add("card-modal-background");
+    document
+      .querySelector("#select-default-date")
+      .classList.add("card-modal-selecting-color");
 
     xButton.addEventListener("click", deleteX);
   });
