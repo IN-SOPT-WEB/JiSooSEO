009a22531c03ffebebbffba0daac6c736c6b4055
Seojisoo20191941 <amy0480@naver.com>
2022-10-14 12:46:34 +0900

    feat : 캐러셀 구현 위해 새로운 파일 생성(기존 index, style, main 복붙)

---
+++ assignment/week2/velog/slideMain.js
@@ -0,0 +1,67 @@
+const dateList = document.querySelector("#select-date-list");
+const dateSelectButton = document.querySelector("#select-default-date");
+
+const selecting = document.querySelectorAll(".selecting");
+const defaultDate = document.querySelector("#select-default-date-text");
+
+function showDateList() {
+  let dateListStatus = dateList.style.display;
+  if (dateListStatus === "block") {
+    document.querySelector("#select-date-list").style.display = "none";
+  } else {
+    document.querySelector("#select-date-list").style.display = "block";
+  }
+  console.log(selecting);
+  for (let i = 0; i < selecting.length; i++) {
+    selecting[i].addEventListener("click", function () {
+      selecting[i].classList.add("date-font-color");
+      defaultDate.innerHTML = selecting[i].innerHTML;
+      document.querySelector("#select-date-list").style.display = "none";
+
+      for (let j = 0; j < selecting.length; j++) {
+        if (j !== i) {
+          selecting[j].classList.remove("date-font-color");
+        }
+      }
+    });
+  }
+}
+
+const cards = document.querySelectorAll(".card");
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("click", function () {
+    console.log("ADfasdfsafdsafds");
+    const cardModal = document.createElement("div");
+    cardModal.setAttribute("class", "card-modal-style");
+    //복사할 카드
+    const cardOrigin = cards[i];
+    //카드 복사하기 - 깊은 복사
+    const cardCopy = cardOrigin.cloneNode(true);
+    cardOrigin.after(cardCopy);
+    //x버튼 만들기
+    const xButton = document.createElement("button");
+    xButton.innerHTML = "x";
+
+    cardModal.appendChild(cardOrigin);
+    cardModal.appendChild(xButton);
+
+    document.body.appendChild(cardModal);
+    document.body.classList.add("card-modal-background");
+    document
+      .querySelector("#select-default-date")
+      .classList.add("card-modal-selecting-color");
+
+    xButton.addEventListener("click", deleteX);
+  });
+}
+function deleteX(e) {
+  const deleteX = e.target.parentElement;
+
+  deleteX.remove();
+  document.body.classList.remove("card-modal-background");
+  document
+    .querySelector("#select-default-date")
+    .classList.remove("card-modal-selecting-color");
+}
+
+dateSelectButton.addEventListener("click", showDateList);
