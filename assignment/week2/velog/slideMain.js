f0553f19f52afbe04fc4bc209c40dfe45701e19c
Seojisoo20191941 <amy0480@naver.com>
2022-10-14 14:24:01 +0900

    refator : 캐러셀 왼오클릭 시 175px씩 이동

--- assignment/week2/velog/slideMain.js
+++ assignment/week2/velog/slideMain.js
@@ -3,19 +3,19 @@ const rightButton = document.querySelector("#right-button");
 const cardContainer = document.querySelector(".card-container");
 
 let clickNum = 0;
+
 function leftAction() {
   if (clickNum === 0) return;
   clickNum -= 1;
 
-  cardContainer.style.transform = `translate3d(${300 * clickNum}px, 0, 0)`;
-  //   cardContainer.style.transform = "translateX(300px)";
+  cardContainer.style.transform = `translate3d(-${175 * clickNum}px, 0, 0)`;
 }
 function rightAction() {
-  if (clickNum === 2) return;
+  if (clickNum === 9) return;
   clickNum += 1;
 
-  cardContainer.style.transform = `translate3d(-${300 * clickNum}px, 0, 0)`;
-  //   cardContainer.style.transform = "translateX(-300px)";
+  cardContainer.style.transform = `translate3d(-${175 * clickNum}px, 0, 0)`;
 }
+
 leftButton.addEventListener("click", leftAction);
 rightButton.addEventListener("click", rightAction);
