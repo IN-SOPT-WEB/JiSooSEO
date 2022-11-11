9acfb41cc16f98864a7d84dbad7e4a23ea81a4d2
Seojisoo20191941 <amy0480@naver.com>
2022-11-11 17:28:49 +0900

    [feat] msw로 지수에 대한 정보페이지 만들기

--- week4/msw/src/App.js
+++ week4/msw/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [jisoos, setTodos] = useState([]);
+  const [jisoo, setTodo] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("/jisoos")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setLoading(true);
+    fetch("jisoos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: jisoo,
+    }).then((res) => {
+      fetch("/jisoos")
+        .then((res) => res.json())
+        .then((data) => {
+          setTodo("");
+          setTodos(data);
+          setLoading(false);
+        });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h2>나는 지수</h2>
+
+      {jisoos.map((jisoo, idx) => (
+        <p key={idx}>{jisoo}</p>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="jisoo"
+          placeholder="지수에 대한 정보"
+          disabled={loading}
+          value={jisoo}
+          onChange={({ target: { value } }) => setTodo(value)}
+        />
+        <button disabled={!jisoo}>추가</button>
+      </form>
+    </section>
   );
 }
 
