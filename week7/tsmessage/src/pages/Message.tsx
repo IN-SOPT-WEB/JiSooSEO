f500d8654b94301944042409bca84664f92003c5
Seojisoo20191941 <amy0480@naver.com>
2022-12-27 23:03:19 +0900

    feat : post 구현

--- week7/tsmessage/src/pages/Message.tsx
+++ week7/tsmessage/src/pages/Message.tsx
@@ -6,6 +6,11 @@ import { List } from '../types/common';
 
 export default function Message() {
     const [messages, setMessages] = useState<List[]>([]);
+    const [isClicked, setIsClicked] = useState<boolean>(false);
+    const [writer, setWriter]=useState<string>("");
+    const [message, setMessage] = useState<string>("");
+    const [password, setPassword]=useState<string>("");
+    const [hint, setHint]=useState<string>("");
 
     const getData = async () => {
         try {
@@ -27,7 +32,32 @@ export default function Message() {
     }, []);
 
 
+    const handleSubmit = (e:any) => {
+        let formData = new FormData();
+        formData.append("writer", writer);
+        formData.append("message", message);
+        formData.append("password", password);
+        formData.append("hint", hint);
+        
+        e.preventDefault();
 
+        axios.post("/letters", 
+            {
+              "writer": writer,
+              "message": message,
+              "password": password,
+              "hint": hint     
+            }
+          ).then((res) => {
+            console.log(res)
+            axios.get("/letters")
+              .then((res) => {
+                setMessage("")
+                setMessages(res.data);
+              });
+          });
+      };
+    
   return (
     <>
         <Header/>
@@ -39,6 +69,14 @@ export default function Message() {
                 </section>
             ))}
 
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="글쓴이" onChange={({ target: { value } }) => setWriter(value)}/>
+            <input type="text" placeholder='메시지 내용' onChange={({ target: { value } }) => setMessage(value)}/>
+            <input type="text" placeholder='비밀번호' onChange={({ target: { value } }) => setPassword(value)}/>
+            <input type="text" placeholder='힌트'  onChange={({ target: { value } }) => setHint(value)}/>
+
+            <button disabled={!message}>추가</button>
+        </form>
 
     </>
   )
