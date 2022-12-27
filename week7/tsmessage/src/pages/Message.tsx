f9bb76866a99307ae38a66ac82ba6bf627296742
Seojisoo20191941 <amy0480@naver.com>
2022-12-27 23:06:12 +0900

    feat : mockserviceworker 추가

--- week7/tsmessage/src/pages/Message.tsx
+++ week7/tsmessage/src/pages/Message.tsx
@@ -3,43 +3,45 @@ import React, { useEffect, useState } from 'react'
 import Header from '../components/common/Header';
 import { List } from '../types/common';
 
-
-export default function Message() {
+export default function MessageList() {
     const [messages, setMessages] = useState<List[]>([]);
     const [isClicked, setIsClicked] = useState<boolean>(false);
-    const [writer, setWriter]=useState<string>("");
-    const [message, setMessage] = useState<string>("");
-    const [password, setPassword]=useState<string>("");
-    const [hint, setHint]=useState<string>("");
+    const [writer, setWriter]=useState("");
+    const [message, setMessage] = useState("");
+    const [password, setPassword]=useState("");
+    const [hint, setHint]=useState("");
 
-    const getData = async () => {
-        try {
-            const res = await axios({
-                method: "get",
-                url: "/letters",
-            });
-            console.log(res.data);
-            if (res.status === 200) {
-                setMessages(res.data);
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
+    const fetchData = async () => {
+      try {
+          const res = await axios({
+              method: "get",
+              url: "/letters",
+          });
+          console.log(res.data);
+          if (res.status === 200) {
+              setMessages(res.data);
+          }
+      } catch (error) {
+          console.log(error);
+      }
+  };
+
 
     useEffect(() => {
-        getData();
+        fetchData();
     }, []);
 
-
-    const handleSubmit = (e:any) => {
+    const handleSubmit = (event:any) => {
         let formData = new FormData();
         formData.append("writer", writer);
         formData.append("message", message);
         formData.append("password", password);
         formData.append("hint", hint);
         
-        e.preventDefault();
+        event.preventDefault();
+        // setMessages("{"writer":writer, "message":message, "password":password, "hint":hint}")
+        console.log("ddfdfdf")
+        console.log(formData)
 
         axios.post("/letters", 
             {
@@ -58,25 +60,32 @@ export default function Message() {
           });
       };
     
+    const handleClick=()=>{
+        setIsClicked((prev)=>!prev)
+    }
+
+
   return (
     <>
         <Header/>
-        <button type='button'>글쓰기</button>
-            {messages.map(({writer, message},i) => (
-                <section key={i}>
-                <p>{writer}</p>
-                <p>{message}</p>
-                </section>
-            ))}
+        {/* {isClicked&&<MessageForm/>} */}
+        <button type='button' onClick={handleClick}>글쓰기</button>
+        {messages.map(({writer, message},i) => (
+            <section key={i}>
+            <p>{writer}</p>
+            <p>{message}</p>
+            </section>
+        ))}
 
-        <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="글쓴이" onChange={({ target: { value } }) => setWriter(value)}/>
-            <input type="text" placeholder='메시지 내용' onChange={({ target: { value } }) => setMessage(value)}/>
-            <input type="text" placeholder='비밀번호' onChange={({ target: { value } }) => setPassword(value)}/>
-            <input type="text" placeholder='힌트'  onChange={({ target: { value } }) => setHint(value)}/>
 
-            <button disabled={!message}>추가</button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="글쓴이" onChange={({ target: { value } }) => setWriter(value)}/>
+        <input type="text" placeholder='메시지 내용' onChange={({ target: { value } }) => setMessage(value)}/>
+        <input type="text" placeholder='비밀번호' onChange={({ target: { value } }) => setPassword(value)}/>
+        <input type="text" placeholder='힌트'  onChange={({ target: { value } }) => setHint(value)}/>
+
+        <button disabled={!message}>추가</button>
+      </form>
 
     </>
   )
